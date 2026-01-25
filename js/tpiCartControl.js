ffc786ead08f7fd54ca6e82c406cf5e7cebc3fd0
unknown <lol_123_lolka_123_lol@mail.ru>
2026-01-25 10:43:10 +0300

    3.19.7_dev11

--- js/tpiCartControl.js
+++ js/tpiCartControl.js
@@ -72,6 +72,16 @@ tpi_cc_i_loading = `
         <animate values="0;256.58892822265625" keyTimes="0;1" dur="0.9523809523809523s" repeatCount="indefinite" attributeName="stroke-dashoffset"/>
     </path>
 </svg>
+`,
+tpi_cc_i_filter_up = `
+<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+    <path d="M240 96h64a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm0 128h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm256 192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-256-64h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm-64 0h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352z"></path>
+</svg>
+`,
+tpi_cc_i_filter_down = `
+<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+    <path d="M240 96h64a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm0 128h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm256 192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-256-64h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm-64 0h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352z"></path>
+</svg>
 `
 
 function checkiIs__onCartControlsPage() {
@@ -128,37 +138,92 @@ function checkiIs__onCartControlsPage() {
                 <thead class="tpi-cc--table-thead-wrapper">
                     <tr class="tpi-cc--table-thead">
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Данные курьера</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Данные курьера</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Ячейка</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Ячейка</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Номер CART</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Номер CART</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Номер PALLET</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Номер PALLET</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Cтатус</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Cтатус</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Прогресс сортировки</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Прогресс сортировки</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Прогресс подготовки</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Прогресс подготовки</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Начало<br>сортировки</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Начало<br>сортировки</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Конец<br>сортировки</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Конец<br>сортировки</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Прибытие<br>курьера</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Прибытие<br>курьера</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                         <th class="tpi-cc--table-thead-item">
-                            <div class="tpi-cc--table-thead-data">Печать</div>
+                            <div class="tpi-cc--table-thead-data">
+                                <p>Печать</p>
+                                <i class="tpi-cc--table-thead-filter">
+                                    ${tpi_cc_i_filter_down}
+                                </i>
+                            </div>
                         </th>
                     </tr>
                 </thead>
