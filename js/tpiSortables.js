3eed803ec3486a9f13a9b17e681eb89aa3ef3aee
unknown <lol_123_lolka_123_lol@mail.ru>
2026-05-28 03:28:28 +0300

    3.21.0_dev2

--- js/tpiSortables.js
+++ js/tpiSortables.js
@@ -2652,6 +2652,7 @@ function initParentSortableObserver() {
                             <div class="diman__scanLog__th__info diman__scanLog__th__icon" is-icons-showed="true">
                                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" fill-rule="evenodd" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="M160 144h304c8.837 0 16 7.163 16 16v304c0 8.837-7.163 16-16 16H160c-8.837 0-16-7.163-16-16V160c0-8.837 7.163-16 16-16m564.314-25.333 181.019 181.02c6.248 6.248 6.248 16.378 0 22.627l-181.02 181.019c-6.248 6.248-16.378 6.248-22.627 0l-181.019-181.02c-6.248-6.248-6.248-16.378 0-22.627l181.02-181.019c6.248-6.248 16.378-6.248 22.627 0M160 544h304c8.837 0 16 7.163 16 16v304c0 8.837-7.163 16-16 16H160c-8.837 0-16-7.163-16-16V560c0-8.837 7.163-16 16-16m400 0h304c8.837 0 16 7.163 16 16v304c0 8.837-7.163 16-16 16H560c-8.837 0-16-7.163-16-16V560c0-8.837 7.163-16 16-16"></path></svg>
                             </div>
+                            <div class="diman__scanLog__th__shadow"></div>
                         </th>`;
         if (operationIndex !== -1) html += `<th class="diman__scanLog__thead__tr__th diman__scanLog__th__stickySection__other">Флоу</th>`;
         if (userIndex !== -1) html += `<th class="diman__scanLog__thead__tr__th diman__scanLog__th__stickySection__other">Кладовщик</th>`;
@@ -2735,6 +2736,7 @@ function initParentSortableObserver() {
                     <div class="diman__scanLog__td__info diman__scanLog__td__date">${date}</div>
                     <div class="diman__scanLog__td__info diman__scanLog__td__time">${time}</div>
                     <div class="diman__scanLog__td__info diman__scanLog__td__icon" is-icons-showed="true"><i${iconAttr}>i</i></div>
+                    </div><div class="diman__scanLog__td__shadow"></div>
                 </td>`;
             if (operationIndex !== -1) html += `<td class="diman__scanLog__tbody__td">${operationCell !== undefined ? operationCell : ''}</td>`;
             if (userIndex !== -1) html += `<td class="diman__scanLog__tbody__td diman__table__short">${userCell !== undefined ? userCell : ''}</td>`;
